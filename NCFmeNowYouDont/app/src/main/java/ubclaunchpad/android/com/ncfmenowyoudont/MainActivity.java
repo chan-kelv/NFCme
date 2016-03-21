5f7c03e1e4c9aed982874713eb848e70d3c0cbbc
KChan <chan.kelv@gmail.com>
2016-03-21 10:56:19 -0700

    added check for nfc. added read/write activities

--- NCFmeNowYouDont/app/src/main/java/ubclaunchpad/android/com/ncfmenowyoudont/MainActivity.java
+++ NCFmeNowYouDont/app/src/main/java/ubclaunchpad/android/com/ncfmenowyoudont/MainActivity.java
@@ -1,7 +1,15 @@
 package ubclaunchpad.android.com.ncfmenowyoudont;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart(){
+        NfcManager manager = (NfcManager) getSystemService(Context.NFC_SERVICE);
+        NfcAdapter adapter = manager.getDefaultAdapter();
+        if (adapter != null && adapter.isEnabled()) {
+            // adapter exists and is enabled.
+        }
+        else{
+            new AlertDialog.Builder(this)
+                    .setTitle("NFC not enabled")
+                    .setMessage("This app requires NFC connection to work")
+                    .setPositiveButton("Turn on NFC", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent setnfc = new Intent(Settings.ACTION_NFC_SETTINGS);
+                            startActivity(setnfc);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setCancelable(false)
+                    .show();
+        }
+        super.onStart();
+    }
+
+    public void writeNFC(View view){
+
+    }
+
+    public void readNFC(View view){
+
     }
 }
